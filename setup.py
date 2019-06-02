84e6c60024f4c839b2d223462b445e49d70f5915
oyanagi1118 <oyanagi1118@ae.auone-net.jp>
2019-06-02 13:29:56 +0900

    setup.py追加

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sample_project_withvenv',
+    version='',
+    packages=['SamplePackage'],
+    url='',
+    license='',
+    author='oyanagi',
+    author_email='',
+    description=''
+)
